87c16522d13019be631516fc29f60c741095ba21
Vandeilsonpp <vandeilson.nobre@picpay.com>
2023-01-18 22:20:38 -0300

    teste com envs

--- src/test/kotlin/com/vandeilson/action/integration/AnotherIT.kt
+++ src/test/kotlin/com/vandeilson/action/integration/AnotherIT.kt
@@ -1,13 +1,28 @@
 package com.vandeilson.action.integration
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.env.Environment
+import org.springframework.test.util.AssertionErrors
 
 @SpringBootTest
 class AnotherIT {
 
+    @Autowired
+    private lateinit var environment: Environment
+
     @Test
-    fun `another intergration test`() {
+    fun `another integration test`() {
         print("My second Integration Test")
+    }
+
+    @Test
+    fun `integration test for envs`() {
+        val testValue = environment.getProperty("INTEGRATION_TEST")
+        AssertionErrors.assertNotNull(testValue!!, "TEST env variable should not be null")
+        println(testValue)
+        println(System.getenv("GLOBAL_TEST"))
+
     }
 }
