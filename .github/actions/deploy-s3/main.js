cd2917cba2e862fc82b981496b045d704d3dec61
Vandeilsonpp <vandeilson.nobre@picpay.com>
2023-01-26 22:32:05 -0300

    Deploy to S3 bucket

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -3,6 +3,18 @@ const github = require('@actions/github');
 
 function run() {
     core.notice('Hello world. This is my custom JS action!')
+
+    // 1 - Get Input Values
+
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+    // 2 - Upload Files
+    const s3Uri = `s3://${bucket}`;
+    const AWS_ACCESS_KEY_ID = 'AKIA6K32I2NLBUVT2UPA';
+    const password = 'WDgpSCYnet0IGRep7PcMYzC1yXr7p6QxcFnkADUF'
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 }
 
 run();
