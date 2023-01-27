bbd1ade170e0ae19fde9a66a3a4fa7f82476b667
Vandeilsonpp <vandeilson.nobre@picpay.com>
2023-01-26 21:53:57 -0300

    rename const

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const core = require('@actions/github');
+const github = require('@actions/github');
 
 function run() {
     core.notice('Hello world. This is my custom JS action!')
